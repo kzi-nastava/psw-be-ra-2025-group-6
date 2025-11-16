8ee19b5ea95b838fc40cc765b53f68c400bed614
Milan Kecman <milankecmanbp@gmail.com>
2025-11-16 13:17:15 +0100

    ReviewApp Entity

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/ReviewApp.cs
@@ -0,0 +1,42 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    public class ReviewApp : Entity
+    {
+        public long UserId { get; init; }
+        public int Rating { get; private set; }
+        public string? Comment { get; private set; }
+        public DateTime CreateAt { get; init; }
+        public DateTime? UpdateAt { get; private set; }
+
+        public ReviewApp(long userId, int rating, string? comment)
+        {
+            UserId = userId;
+            Rating = rating;
+            Comment = comment;
+            CreateAt = DateTime.UtcNow;
+            UpdateAt = null;
+            Validate();
+        }
+
+        public void Update(int rating, string? comment)
+        {
+            Rating = rating;
+            Comment = comment;
+            UpdateAt = DateTime.UtcNow;
+            Validate();
+        }
+        private ReviewApp() { }
+
+        private void Validate()
+        {
+            if (Rating < 1 || Rating > 5) throw new ArgumentException("Rating must be between 1 and 5.");
+        }
+    }
+}
