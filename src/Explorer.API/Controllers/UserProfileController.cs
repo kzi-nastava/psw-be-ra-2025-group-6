09f9fb836d86f969b918dada5dd7f45e1aae088b
Strahinja Ponjevic <strahinjaponjevic03@gmail.com>
2025-11-17 17:18:46 +0100

    feat: UserProfile controller

---
+++ src/Explorer.API/Controllers/UserProfileController.cs
@@ -0,0 +1,47 @@
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Explorer.API.Controllers
+{
+    [Authorize(Policy = "registeredUserPolicy")]
+    [Route("api/profile")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserProfileService _userProfileService;
+
+        public UserProfileController(IUserProfileService userProfileService)
+        {
+            _userProfileService = userProfileService;
+        }
+
+        [HttpGet]
+        public ActionResult<UserProfileDto> Get()
+        {
+            var userIdClaim = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userIdClaim))
+            {
+                return BadRequest("User ID not found in token.");
+            }
+            var userId = long.Parse(userIdClaim);
+            var result = _userProfileService.Get(userId);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public ActionResult<UserProfileDto> Update([FromBody] UserProfileDto profile)
+        {
+            var userIdClaim = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userIdClaim))
+            {
+                return BadRequest("User ID not found in token.");
+            }
+            profile.UserId = long.Parse(userIdClaim);
+            var result = _userProfileService.Update(profile);
+            return Ok(result);
+        }
+    }
+}
