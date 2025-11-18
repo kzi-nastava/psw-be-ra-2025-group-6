13f7341c19163a4ebd07e7cd8a41d1ddde93b316
Dusan <proba@nesto.com>
2025-11-18 17:43:08 +0100

    feat: Implement full CRUD functionality for Clubs

---
+++ src/Explorer.API/Controllers/ClubController.cs
@@ -0,0 +1,54 @@
+﻿using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Explorer.API.Controllers
+{
+    [Authorize(Policy = "touristPolicy")]
+    [Route("api/community/clubs")]
+    public class ClubController : ControllerBase
+    {
+        private readonly IClubService _clubService;
+
+        public ClubController(IClubService clubService)
+        {
+            _clubService = clubService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<ClubDto>> GetAll()
+        {
+            var result = _clubService.GetAll();
+            return Ok(result);
+        }
+
+        [HttpGet("{id:long}")]
+        public ActionResult<ClubDto> Get(long id)
+        {
+            var result = _clubService.Get(id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public ActionResult<ClubDto> Create([FromBody] ClubDto club)
+        {
+            var result = _clubService.Create(club);
+            return Ok(result);
+        }
+
+        [HttpPut("{id:long}")]
+        public ActionResult<ClubDto> Update([FromBody] ClubDto club)
+        {
+            var result = _clubService.Update(club);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:long}")]
+        public ActionResult Delete(long id)
+        {
+            _clubService.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/Repositories/ClubDbRepository.cs
@@ -0,0 +1,63 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.Core.Domain;
+using Explorer.Stakeholders.Core.Domain.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore; 
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Explorer.Stakeholders.Infrastructure.Database.Repositories
+{
+    public class ClubDbRepository : IClubRepository
+    {
+        private readonly StakeholdersContext _dbContext;
+        private readonly DbSet<Club> _dbSet;
+
+        public ClubDbRepository(StakeholdersContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<Club>();
+        }
+
+        public Club Create(Club club)
+        {
+            _dbSet.Add(club);
+            _dbContext.SaveChanges();
+            return club;
+        }
+
+        public void Delete(long id)
+        {
+            var entity = _dbSet.Find(id);
+            if (entity == null) throw new KeyNotFoundException("Not found: " + id);
+            _dbSet.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public Club Get(long id)
+        {
+            var entity = _dbSet.Find(id);
+            if (entity == null) throw new KeyNotFoundException("Not found: " + id);
+            return entity;
+        }
+
+        public List<Club> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public Club Update(Club club)
+        {
+            try
+            {
+                _dbContext.Update(club);
+
+                _dbContext.SaveChanges();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new KeyNotFoundException(e.Message);
+            }
+            return club;
+        }
+    }
+}
