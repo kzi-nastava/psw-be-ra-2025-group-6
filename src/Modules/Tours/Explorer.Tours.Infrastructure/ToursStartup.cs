752cf25eb73be644a988b2a8154b0257af822fa7
Pavicevic Tea <pavicevictea6@gmail.com>
2025-11-18 20:02:42 +0100

    feat:Monument repository added, DbContext updated and DI configured

--- src/Modules/Tours/Explorer.Tours.Infrastructure/ToursStartup.cs
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/ToursStartup.cs
@@ -25,11 +25,13 @@ public static class ToursStartup
     private static void SetupCore(IServiceCollection services)
     {
         services.AddScoped<IEquipmentService, EquipmentService>();
+        services.AddScoped<IMonumentService, MonumentService>();
     }
 
     private static void SetupInfrastructure(IServiceCollection services)
     {
         services.AddScoped<IEquipmentRepository, EquipmentDbRepository>();
+        services.AddScoped<IMonumentRepository, MonumentDbRepository>();
 
         var dataSourceBuilder = new NpgsqlDataSourceBuilder(DbConnectionStringBuilder.Build("tours"));
         dataSourceBuilder.EnableDynamicJson();
