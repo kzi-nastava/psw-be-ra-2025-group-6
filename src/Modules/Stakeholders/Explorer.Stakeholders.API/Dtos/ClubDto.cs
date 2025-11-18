bd07c5b5af5cc976dc1e663a74fbeb3af9fb29e6
Dusan <proba@nesto.com>
2025-11-18 11:40:39 +0100

    feat: Implement service and API layers for Clubs

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/ClubDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Explorer.Stakeholders.API.Dtos
+{
+    public class ClubDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> ImageUris { get; set; }
+        public long OwnerId { get; set; }
+    }
+}
