2ee976c9031cb916fa8dd4532a30894c35825a0f
Marina Medic <marinamedic25@gmail.com>
2025-11-21 21:09:29 +0100

    feat: sql conflict resolved

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-﻿DELETE FROM tours."Equipment";
-
+DELETE FROM tours."Equipment";
 DELETE FROM tours."AnnualAwards";
-=======
-
-﻿DELETE FROM tours."Equipment";
 DELETE FROM tours."Tours";
 DELETE FROM tours."Monuments";
-﻿DELETE FROM tours."TourProblems";
-
->>>>>>> development-new
+DELETE FROM tours."TourProblems";
