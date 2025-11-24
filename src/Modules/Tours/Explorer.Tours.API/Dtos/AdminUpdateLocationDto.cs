c465ee7a899598bd6baf43aaa7bf5e190bb6a8af
Pavicevic Tea <pavicevictea6@gmail.com>
2025-11-24 22:29:10 +0100

    feat: new needed dto classes added

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/AdminUpdateLocationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Explorer.Tours.API.Dtos
+{
+    public class AdminUpdateLocationDto
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
