dbd0e2ae2c41efe96b7b9d6980c6121a44b5f4b7
aurenaa <salapuranev@gmail.com>
2025-11-25 20:49:04 +0100

    fix : Admin can create blog in Swagger fix

--- src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Authentication/ClaimsPrincipalExtensions.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Authentication/ClaimsPrincipalExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using Explorer.Stakeholders.Core.Domain;
+using System.Security.Claims;
 
 namespace Explorer.Stakeholders.Infrastructure.Authentication;
 
@@ -6,4 +7,13 @@ public static class ClaimsPrincipalExtensions
 {
     public static long PersonId(this ClaimsPrincipal user)
         => long.Parse(user.Claims.First(i => i.Type == "personId").Value);
+
+    public static UserRole Role(this ClaimsPrincipal user)
+    {
+        var roleClaim = user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
+        if (roleClaim == null)
+            throw new Exception("Role claim not found");
+
+        return Enum.Parse<UserRole>(roleClaim);
+    }
 }
