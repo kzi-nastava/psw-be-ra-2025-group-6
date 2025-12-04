87f9400f1ca4f0a5a9d3b881b35a24b51dffe059
adorottya <apro.ra118.2022@uns.ac.rs>
2025-12-04 19:13:45 +0100

    feat: Nove obelezje TourReviewa Procenat kompletnosti ture

--- src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/TourReviewDto.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/TourReviewDto.cs
@@ -6,6 +6,7 @@
         public long UserId { get; init; }
         public long TourId { get; init; }
         public int Rating { get; set; }
+        public int CompletedPercent { get; set; }
         public string? Comment { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/TourReview.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/TourReview.cs
@@ -8,11 +8,11 @@ public class TourReview : Entity
     public long TourId { get; private set; }
     public int Rating { get; private set; }
     public string? Comment { get; private set; }
+    public int CompletedPercent { get; private set; }
     public DateTime CreatedAt { get; private set; }
     public DateTime? UpdatedAt { get; private set; }
 
-
-    public TourReview(long userId, long tourId, int rating, string? comment)
+    public TourReview(long userId, long tourId, int rating, string? comment, int completedPercent)
     {
         UserId = userId;
         TourId = tourId;
@@ -20,6 +20,7 @@ public class TourReview : Entity
         Comment = comment;
         CreatedAt = DateTime.UtcNow;
         UpdatedAt = null;
+        CompletedPercent = completedPercent;
         Validate();
     }
 
@@ -27,5 +28,6 @@ public class TourReview : Entity
     {
         if (UserId == 0) throw new ArgumentException("Invalid PersonId");
         if (Rating < 0 || Rating > 5) throw new ArgumentException("Rating must be between 1 and 5.");
+        if (CompletedPercent < 0 || CompletedPercent > 100) throw new ArgumentException("CompletedPercent must be between 0 and 100.");
     }
 }
