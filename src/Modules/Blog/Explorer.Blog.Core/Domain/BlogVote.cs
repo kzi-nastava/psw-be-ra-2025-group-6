718a937bde92828e564f7b24d796ff04f368d848
Pavicevic Tea <pavicevictea6@gmail.com>
2025-12-04 03:16:27 +0100

    feat: new needed domain classes added

---
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/BlogVote.cs
@@ -0,0 +1,30 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Blog.Core.Domain
+{
+    public class BlogVote : Entity
+    {
+        public long BlogPostId { get; private set; }
+        public long UserId { get; private set; }
+        public DateTime VotedAt { get; private set; }
+        public VoteType Type { get; private set; }
+
+        private BlogVote() { }
+
+        public BlogVote(long blogPostId, long userId, VoteType type)
+        {
+            if (blogPostId == 0) throw new ArgumentException("Invalid BlogPostId.");
+            if (userId == 0) throw new ArgumentException("Invalid UserId.");
+            BlogPostId = blogPostId;
+            UserId = userId;
+            Type = type;
+            VotedAt = DateTime.UtcNow;
+        }
+
+        public void UpdateVote(VoteType newType)
+        {
+            Type = newType;
+            VotedAt = DateTime.UtcNow;
+        }
+    }
+}
