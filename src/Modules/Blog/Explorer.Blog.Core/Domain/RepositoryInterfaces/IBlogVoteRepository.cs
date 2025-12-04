efa4c15d034892c9556e3de9cbc04d270e666854
Pavicevic Tea <pavicevictea6@gmail.com>
2025-12-04 03:28:01 +0100

    feat: repository interface and dbrepository class added

---
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/RepositoryInterfaces/IBlogVoteRepository.cs
@@ -0,0 +1,14 @@
+﻿using Explorer.Blog.Core.Domain;
+
+namespace Explorer.Blog.Core.Domain.RepositoryInterfaces
+{
+    public interface IBlogVoteRepository
+    {
+        BlogVote GetByUserAndBlog(long userId, long blogPostId);
+        void Create(BlogVote vote);
+        void Update(BlogVote vote);
+        void Delete(BlogVote vote);
+        int CountUpvotes(long blogPostId);
+        int CountDownvotes(long blogPostId);
+    }
+}
