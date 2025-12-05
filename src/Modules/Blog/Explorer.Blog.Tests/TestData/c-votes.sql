c3cf3df09c0b60635274110facb621a185a97389
Pavicevic Tea <pavicevictea6@gmail.com>
2025-12-05 18:58:07 +0100

    tests: needed tests added

---
+++ src/Modules/Blog/Explorer.Blog.Tests/TestData/c-votes.sql
@@ -0,0 +1,8 @@
+INSERT INTO blog."Votes"("Id", "BlogId", "UserId", "Value", "CreatedAt")
+VALUES (-100, -1, -11, 1, NOW());
+
+INSERT INTO blog."Votes"("Id", "BlogId", "UserId", "Value", "CreatedAt")
+VALUES (-101, -1, -12, -1, NOW());
+
+INSERT INTO blog."Votes"("Id", "BlogId", "UserId", "Value", "CreatedAt")
+VALUES (-102, -3, -11, 1, NOW());
