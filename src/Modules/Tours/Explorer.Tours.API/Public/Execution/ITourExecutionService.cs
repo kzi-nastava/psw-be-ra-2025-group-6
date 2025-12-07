78ca7a4e2c8a63346d432852e5e275a9a21ca991
gagicanja <gagic.ra165.2022@uns.ac.rs>
2025-12-07 19:29:56 +0100

    Create ITourExecutionService.cs

---
+++ src/Modules/Tours/Explorer.Tours.API/Public/Execution/ITourExecutionService.cs
@@ -0,0 +1,9 @@
+using Explorer.Tours.API.Dtos;
+
+namespace Explorer.Tours.API.Public.Execution;
+
+public interface ITourExecutionService
+{
+    TourExecutionStartResultDto StartExecution(TourExecutionStartDto dto, long touristId);
+    TourExecutionStartResultDto? GetActiveExecution(long touristId, long? tourId = null);
+}
