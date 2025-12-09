61219d474201c37de24c42cbfeadc60c24aaef4e
zebi30 <acazebeljanaca@gmail.com>
2025-12-09 17:20:08 +0100

    feat: Added necessary integration tests and SQL seed scripts

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/b-tours.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/b-tours.sql
@@ -6,3 +6,6 @@ VALUES (-2, 'Tura Beograda', '', 0, ARRAY[]::text[], 0, 0);
 
 INSERT INTO tours."Tours"("Id", "Name", "Description", "Difficulty", "Tags", "Price", "Status")
 VALUES (-3, 'Tura Pariza', 'Pravo u Luvr', 0, ARRAY['europe', '7 days'], 100, 1);
+
+INSERT INTO tours."Tours" ("Id", "Name", "Description", "Difficulty", "Tags", "Price", "Status")
+VALUES (-4, 'Another Confirmed Tour', 'Another confirmed tour', 2, ARRAY['test'], 150, 1);
