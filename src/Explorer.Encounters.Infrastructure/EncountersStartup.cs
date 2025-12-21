767a6e370a2bfc1a761b8f19cc2148cede738eed
Aleksandra <aleksandrarudez12@gmail.com>
2025-12-21 23:59:10 +0100

    feat: complete initialization and integration of Encounters module

---
+++ src/Explorer.Encounters.Infrastructure/EncountersStartup.cs
@@ -0,0 +1,31 @@
+﻿using Explorer.Encounters.Core.Mappers;
+using Explorer.Encounters.Infrastructure.Database; 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Explorer.BuildingBlocks.Infrastructure.Database;
+
+namespace Explorer.Encounters.Infrastructure
+{
+    public static class EncountersStartup
+    {
+        public static IServiceCollection ConfigureEncountersModule(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(EncountersProfile).Assembly);
+            SetupCore(services);
+            SetupInfrastructure(services);
+            return services;
+        }
+
+        private static void SetupCore(IServiceCollection services)
+        {
+            // Dodati core servise ovde ako je potrebno
+        }
+
+        private static void SetupInfrastructure(IServiceCollection services)
+        {
+            services.AddDbContext<EncountersContext>(opt =>
+                opt.UseNpgsql(DbConnectionStringBuilder.Build("encounters"),
+                    x => x.MigrationsHistoryTable("__EFMigrationsHistory", "encounters")));
+        }
+    }
+}
