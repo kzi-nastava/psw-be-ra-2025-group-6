5746ffb8508d2b838ec5e321bb966cd8e005bd3a
gagicanja <gagic.ra165.2022@uns.ac.rs>
2025-12-24 15:16:53 +0100

    feat: implement Challenge repository and persistence

--- src/Explorer.Encounters.Infrastructure/Database/EncountersContext.cs
+++ src/Explorer.Encounters.Infrastructure/Database/EncountersContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Explorer.Encounters.Core.Domain;
 
 namespace Explorer.Encounters.Infrastructure.Database
 {
@@ -11,9 +12,20 @@ namespace Explorer.Encounters.Infrastructure.Database
     {
         public EncountersContext(DbContextOptions<EncountersContext> options) : base(options) { }
 
+        public DbSet<Challenge> Challenges { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasDefaultSchema("encounters");
+
+            modelBuilder.Entity<Challenge>(b =>
+            {
+                b.HasKey(c => c.Id);
+                b.Property(c => c.Title).IsRequired();
+                b.Property(c => c.Description).IsRequired();
+                b.Property(c => c.Status).HasConversion<string>();
+                b.Property(c => c.Type).HasConversion<string>();
+            });
         }
     }
 }
