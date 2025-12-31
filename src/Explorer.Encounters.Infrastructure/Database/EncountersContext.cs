8ffa904ab7707712ee8f35aa6d7d6aa6e3da57d2
gagicanja <gagic.ra165.2022@uns.ac.rs>
2025-12-31 11:32:01 +0100

    feat: adding xp and level changes to encounters

--- src/Explorer.Encounters.Infrastructure/Database/EncountersContext.cs
+++ src/Explorer.Encounters.Infrastructure/Database/EncountersContext.cs
@@ -13,6 +13,8 @@ namespace Explorer.Encounters.Infrastructure.Database
         public EncountersContext(DbContextOptions<EncountersContext> options) : base(options) { }
 
         public DbSet<Challenge> Challenges { get; set; }
+        public DbSet<TouristXpProfile> TouristXpProfiles { get; set; }
+        public DbSet<EncounterCompletion> EncounterCompletions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -25,6 +27,28 @@ namespace Explorer.Encounters.Infrastructure.Database
                 b.Property(c => c.Description).IsRequired();
                 b.Property(c => c.Status).HasConversion<string>();
                 b.Property(c => c.Type).HasConversion<string>();
+                b.Property(c => c.CreatorId);
+                b.Property(c => c.IsCreatedByTourist);
+            });
+
+            modelBuilder.Entity<TouristXpProfile>(b =>
+            {
+                b.HasKey(p => p.Id);
+                b.Property(p => p.UserId).IsRequired();
+                b.Property(p => p.CurrentXP).IsRequired();
+                b.Property(p => p.Level).IsRequired();
+                b.Property(p => p.LevelUpHistory).HasColumnType("jsonb");
+                b.HasIndex(p => p.UserId).IsUnique();
+            });
+
+            modelBuilder.Entity<EncounterCompletion>(b =>
+            {
+                b.HasKey(c => c.Id);
+                b.Property(c => c.UserId).IsRequired();
+                b.Property(c => c.ChallengeId).IsRequired();
+                b.Property(c => c.CompletedAt).IsRequired();
+                b.Property(c => c.XpAwarded).IsRequired();
+                b.HasIndex(c => new { c.UserId, c.ChallengeId }).IsUnique();
             });
         }
     }
