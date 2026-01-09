e6d78a6d98e3f38fa650e334c87c4f7e3a1a368b
gagicanja <gagic.ra165.2022@uns.ac.rs>
2026-01-09 11:11:29 +0100

    fix: adding changes to seed script

--- src/Explorer.Encounters.Tests/TestData/b-challenges.sql
+++ src/Explorer.Encounters.Tests/TestData/b-challenges.sql
@@ -1,5 +1,5 @@
 -- Seed challenges
-INSERT INTO encounters."Challenges" ("Title", "Description", "Longitude", "Latitude", "XP", "Status", "Type") VALUES
-('Active Challenge 1', 'Desc 1', 19.84, 45.25, 50, 'Active', 'Location'),
-('Draft Challenge 1', 'Desc 2', 19.85, 45.26, 30, 'Draft', 'Social'),
-('Archived Challenge 1', 'Desc 3', 19.86, 45.27, 20, 'Archived', 'Misc');
+INSERT INTO encounters."Challenges" ("Title", "Description", "Longitude", "Latitude", "XP", "Status", "Type", "CreatorId", "IsCreatedByTourist") VALUES
+('Active Challenge 1', 'Desc 1', 19.84, 45.25, 50, 'Active', 'Location', NULL, false),
+('Draft Challenge 1', 'Desc 2', 19.85, 45.26, 30, 'Draft', 'Social', NULL, false),
+('Archived Challenge 1', 'Desc 3', 19.86, 45.27, 20, 'Archived', 'Misc', NULL, false);
