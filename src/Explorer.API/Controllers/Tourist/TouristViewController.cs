515f8fac0c8ae0e9f271d1038e75fc101b9230c9
zebi30 <acazebeljanaca@gmail.com>
2026-02-05 12:05:12 +0100

    fix: fixed conflict error in touristviewcontroller

--- src/Explorer.API/Controllers/Tourist/TouristViewController.cs
+++ src/Explorer.API/Controllers/Tourist/TouristViewController.cs
@@ -39,6 +39,8 @@ namespace Explorer.Tours.API.Controllers.Tourist
             var touristId = User.PersonId();
             var tours = _tourService.GetAvailableForTouristPaged(touristId, page, pageSize);
             return Ok(tours);
+        }
+
         [HttpGet("{id:long}/key-points")]
         public ActionResult<List<KeyPointDto>> GetKeyPoints(long id)
         {
